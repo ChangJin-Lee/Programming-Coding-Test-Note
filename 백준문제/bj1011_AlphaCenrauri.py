602affe0122fb4aea40a0c5a8d616efd7e0319ae
changjinlee <54494793+Jotter-Vortex@users.noreply.github.com>
2021-08-27 23:05:10 +0900

    bj1011_AlphaCenrauri

--- 백준문제/bj1011_AlphaCenrauri.py
+++ 백준문제/bj1011_AlphaCenrauri.py
@@ -1,11 +1,15 @@
+# 1+2+...+n+n-1+...+2+1 = n^2 임을 이용하는 문제.
 import sys
 T = int(sys.stdin.readline().rstrip())
 for _ in range(T):
     cnt=0
     x,y = map(int,sys.stdin.readline().rstrip().split())
     n = int((y-x)**0.5)
-    cnt+=2*n+1
+    cnt+=2*n-1
     square=n**2
-    cnt+= int((y-square)/n)
-    if (y-square)%n != 0 : cnt+=1
+    cnt+= int((y-x-square)/n)
+    if (y-x-square)%n != 0 : cnt+=1
     print(cnt)
+
+# 숏코딩
+# for i in[int]*int(input()):x,y=input().split();print(i(2*(i(y)-i(x)-.5)**.5))
