1a7af278b53bf84669ca67160931596b98799fbf
Jotter-Vortex <ckdwls525@gmail.com>
2021-08-29 22:13:17 +0900

    main

--- main.py
+++ main.py
@@ -3,20 +3,16 @@ import sys
 def prime_list(n):
     # 에라토스테네스의 체 초기화: n개 요소에 True 설정(소수로 간주)
     sieve = [True] * n
-
+    
+    k=n
     m = int(n ** 0.5)
     for i in range(2, m + 1):
+        if k==1: break
         if sieve[i] == True:           # i가 소수인 경우 
+            while k%i==0: k/=i; print(i)
             for j in range(i+i, n, i): # i이후 i의 배수들을 False 판정
                 sieve[j] = False
-    return [i for i in range(2, n) if sieve[i] == True]
-
+    if k!=1: print(int(k))
+    return 1
 N = int(sys.stdin.readline().rstrip())
-primes = prime_list(N)
-primes.append(N)
-for prime in primes:
-    while N%prime == 0: 
-        N=N/prime
-        print(prime)
-        if N==1: break
-    if N==1: break
+prime_list(N)
