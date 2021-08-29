c6aa18ea7da6396563e63f6e281407682ee25f00
Jotter-Vortex <ckdwls525@gmail.com>
2021-08-29 22:20:15 +0900

    bj11653_소인수분해

---
+++ 백준문제/bj11653_소인수분해.py
@@ -0,0 +1,19 @@
+import sys
+# 자연수 N보다 작은 소수들을 구함
+# 에라토네스의 체 돌리면서 출력함
+def eratos(n):
+    # 에라토스테네스의 체 초기화: n개 요소에 True 설정(소수로 간주)
+    sieve = [True] * n
+    
+    k=n
+    m = int(n ** 0.5)
+    for i in range(2, m + 1):
+        if k==1: break
+        if sieve[i] == True:           # i가 소수인 경우 
+            while k%i==0: k/=i; print(i)
+            for j in range(i+i, n, i): # i이후 i의 배수들을 False 판정
+                sieve[j] = False
+    if k!=1: print(int(k))
+    return 1
+N = int(sys.stdin.readline().rstrip())
+eratos(N)
