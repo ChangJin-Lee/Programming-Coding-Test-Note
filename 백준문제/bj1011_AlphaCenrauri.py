cd4ec39a3141dd61ad0739060edba08ad28681d7
Jotter-Vortex <ckdwls525@gmail.com>
2021-08-29 16:04:26 +0900

    ㅁ

--- 백준문제/bj1011_AlphaCenrauri.py
+++ 백준문제/bj1011_AlphaCenrauri.py
@@ -4,8 +4,12 @@ for _ in range(T):
     cnt=0
     x,y = map(int,sys.stdin.readline().rstrip().split())
     n = int((y-x)**0.5)
-    cnt+=2*n+1
+    # print(n)
+    cnt+=2*n-1
+    # print(cnt)
     square=n**2
-    cnt+= int((y-square)/n)
-    if (y-square)%n != 0 : cnt+=1
+    # print(square)
+    if y-x-square > 0:
+        if int((y-x-square)/n) == 0 : cnt+=1
+        else: cnt+= int((y-x-square)/n)+1
     print(cnt)
