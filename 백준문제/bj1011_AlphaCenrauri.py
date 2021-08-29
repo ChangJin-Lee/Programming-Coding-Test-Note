f977afecd4f8eb9b4cc8e5a7c78486b593408a00
Jotter-Vortex <ckdwls525@gmail.com>
2021-08-29 16:10:56 +0900

    centauri

--- 백준문제/bj1011_AlphaCenrauri.py
+++ 백준문제/bj1011_AlphaCenrauri.py
@@ -5,21 +5,19 @@ for _ in range(T):
     cnt=0
     x,y = map(int,sys.stdin.readline().rstrip().split())
     n = int((y-x)**0.5)
-<<<<<<< HEAD
-    # print(n)
-    cnt+=2*n-1
-    # print(cnt)
-    square=n**2
-    # print(square)
-    if y-x-square > 0:
-        if int((y-x-square)/n) == 0 : cnt+=1
-        else: cnt+= int((y-x-square)/n)+1
-=======
+# <<<<<<< HEAD
+#     # print(n)
+#     cnt+=2*n-1
+#     # print(cnt)
+#     square=n**2
+#     # print(square)
+#     if y-x-square > 0:
+#         if int((y-x-square)/n) == 0 : cnt+=1
+#         else: cnt+= int((y-x-square)/n)+1
     cnt+=2*n-1
     square=n**2
     cnt+= int((y-x-square)/n)
     if (y-x-square)%n != 0 : cnt+=1
->>>>>>> 2bd9f5de8a960d3b49e39b230cbc244382b01101
     print(cnt)
 
 # 숏코딩
