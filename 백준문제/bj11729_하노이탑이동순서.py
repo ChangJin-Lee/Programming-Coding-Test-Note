e14e087719dd195edec2265c6c1a1ae3367c0330
Jotter-Vortex <ckdwls525@gmail.com>
2021-09-05 19:46:32 +0900

    bj11729_하노이탑이동순서

--- 백준문제/bj11729_하노이탑이동순서.py
+++ 백준문제/bj11729_하노이탑이동순서.py
@@ -1,5 +1,7 @@
 import sys
-
+# T(n)을 원판 n개를 첫 번째 장대에서 세 번째 장대로 옮기는데 드는 시간
+# T(n) = T(n-1) + T(n-1) + 1 ( n>1)
+# T(n) = 1 (n==1)
 def T_H(A,B,C,n,list):
     if(n<=0): return 0
     T_H(A,C,B,n-1,list)
