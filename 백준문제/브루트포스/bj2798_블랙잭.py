38c9e3130f6f302540a8140f9a842efec3a2c31a
Jotter-Vortex <ckdwls525@gmail.com>
2021-09-05 20:02:59 +0900

    bj2798_블랙잭

--- 백준문제/브루트포스/bj2798_블랙잭.py
+++ 백준문제/브루트포스/bj2798_블랙잭.py
@@ -1,1 +1,11 @@
+import sys
+N, M = map(int,sys.stdin.readline().rstrip().split())
+cards = list(map(int,sys.stdin.readline().rstrip().split()))
+sums = 0
+for i in range(N):
+    for j in range(i+1,N):
+        for k in range(j+1,N):
+            compare = cards[i]+cards[j]+cards[k]
+            if compare > sums and compare <= M: sums = compare
+print(sums)
 
