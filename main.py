f4ac688a722192e2733c1e8add05acc9f5022b88
Chang-Jin Lee <54494793+Jotter-Vortex@users.noreply.github.com>
2021-09-12 05:22:31 +0000

    Bj1018_체스판

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 import sys
-# 자연수 N보다 작은 소수들을 구함
-def prime_list(n):
-    # 에라토스테네스의 체 초기화: n개 요소에 True 설정(소수로 간주)
-    sieve = [True] * n
-    
-    k=n
-    m = int(n ** 0.5)
-    for i in range(2, m + 1):
-        if k==1: break
-        if sieve[i] == True:           # i가 소수인 경우 
-            while k%i==0: k/=i; print(i)
-            for j in range(i+i, n, i): # i이후 i의 배수들을 False 판정
-                sieve[j] = False
-    if k!=1: print(int(k))
-    return 1
-N = int(sys.stdin.readline().rstrip())
-prime_list(N)
+m,n = map(int, sys.stdin.readline().rstrip().split())
+datas,cnt, countercnt= [],[], m*n
+for _ in range(m): datas.append(list(sys.stdin.readline().rstrip()))
+for p in range(m-7):
+    for q in range(n-7):
+        startP = datas[p][q]
+        # print(datas)
+        ccnt=0
+        for i in range(p,p+8,2):
+            for j in range(q,q+8,2):
+                if datas[i][j] != startP : ccnt+=1
+            for j in range(q+1,q+8,2):
+                if datas[i][j] == startP : ccnt+=1
+                # print(datas[i][j],startP,cnt)
+        for i in range(p+1,p+8,2):
+            for j in range(q,q+8,2):
+                if datas[i][j] == startP : ccnt+=1
+            for j in range(q+1,q+8,2):
+                if datas[i][j] != startP : ccnt+=1
+                # print(datas[i][j],startP,cnt)
+        cnt.append(min(ccnt,countercnt))
+print(min(cnt))
+
+
--- 백준문제/브루트포스/bj1018_체스판다시칠하기.py
+++ 백준문제/브루트포스/bj1018_체스판다시칠하기.py
@@ -1,6 +1,6 @@
 import sys
 m,n = map(int, sys.stdin.readline().rstrip().split())
-datas, cnt= [], m*n
+datas,cnt, countercnt= [],[], m*n
 for _ in range(m): datas.append(list(sys.stdin.readline().rstrip()))
 for p in range(m-7):
     for q in range(n-7):
@@ -19,6 +19,7 @@ for p in range(m-7):
             for j in range(q+1,q+8,2):
                 if datas[i][j] != startP : ccnt+=1
                 # print(datas[i][j],startP,cnt)
-        if ccnt < cnt : cnt = ccnt
+        cnt.append(min(ccnt,countercnt))
+print(min(cnt))
 
 
