3abfbd6f8b86305c6fb4d5736ec491db6de628a4
Jotter-Vortex <ckdwls525@gmail.com>
2021-09-12 20:23:56 +0900

    bj11651_좌표정렬하기2

---
+++ 백준문제/정렬/bj11651_좌표정렬하기2.py
@@ -0,0 +1,9 @@
+import sys
+n = int(sys.stdin.readline().rstrip())
+points = []
+for _ in range(n):
+    points.append(list(map(int,reversed(sys.stdin.readline().rstrip().split()))))
+points=sorted(points)
+for point in points:
+    print(" ".join(map(str,reversed(point))))
+
