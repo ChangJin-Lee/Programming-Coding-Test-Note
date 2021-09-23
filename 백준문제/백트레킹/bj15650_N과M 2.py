cbf608a5f2b5904c60cada649c3a29d657202d91
changjinlee <54494793+Jotter-Vortex@users.noreply.github.com>
2021-09-23 10:49:25 +0900

    bj15650_N과M 2

---
+++ 백준문제/백트레킹/bj15650_N과M 2.py
@@ -0,0 +1,10 @@
+import sys
+import itertools
+N,M = map(int,sys.stdin.readline().rstrip().split())
+numbers=[]
+for i in range(N):
+    numbers.append(i+1)
+# nCm
+permutations = list(itertools.combinations(numbers,M))
+for permutation in permutations:
+    print(' '.join(map(str,permutation)))
