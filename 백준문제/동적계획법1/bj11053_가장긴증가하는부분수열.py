1db27ef340eb02157c69d0d5412630acf5cc839a
Chang-Jin Lee <54494793+Jotter-Vortex@users.noreply.github.com>
2021-11-02 10:54:05 +0900

    bj11053_가장긴증가하는부분수열.py

--- 백준문제/동적계획법1/bj11053_가장긴증가하는부분수열.py
+++ 백준문제/동적계획법1/bj11053_가장긴증가하는부분수열.py
@@ -3,7 +3,9 @@ import sys
 
 n = int(sys.stdin.readline().rstrip())
 a = list(map(int, sys.stdin.readline().split()))
+a2 = list(reversed(a))
 dp = [1 for i in range(1001)]
+dp2 = [1 for i in range(1001)]
 result = 0
 
 
@@ -13,10 +15,18 @@ for i in range(1,n):
         if a[i] > a[j] and dp[j] >= max:
             max = dp[j]
             dp[i] = max+1
+    max = 0
+    for j in range(i):
+        if a2[i] > a2[j] and dp2[j] >= max:
+            max = dp2[j]
+            dp2[i] = max + 1
+
 
 for i in range(n):
+    dp[i] = dp[i] + dp2[n-1-i]
     if result < dp[i]:
         result = dp[i]
-
+#
 # print(dp)
-print(result)
+# print(dp2)
+print(result-1)
