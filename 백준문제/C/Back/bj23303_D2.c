00ca98d96609a31e83d6625153b384ca1a142200
changjinlee <54494793+Jotter-Vortex@users.noreply.github.com>
2022-02-15 12:51:49 +0900

    Code review
    Code review

--- 백준문제/C/Back/bj23303_D2.c
+++ 백준문제/C/Back/bj23303_D2.c
@@ -2,11 +2,13 @@
 #include<stdio.h>
 #include<string.h>
 
+// not exist bool variable in C lang, so we need to define bool
 typedef enum{
 	False,
 	True
 }bool;
 
+// check input that char array. if exist "D2" or "d2" in char array, then return True. 
 bool Checkstr(char str[]) {
 	bool check = False;
 	int strlength = strlen(str);
@@ -21,6 +23,7 @@ bool Checkstr(char str[]) {
 
 int main() {
 	char strings[10000];
+	// why gets? -> because end point. blank point
 	gets(strings);
 	if (Checkstr(strings)) printf("D2");
 	else printf("unrated");
