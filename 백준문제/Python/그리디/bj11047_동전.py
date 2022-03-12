53cca14b8ad10f662b9edb370e44e9cc7fa2168a
Jotter-Vortex <ckdwls525@gmail.com>
2022-03-12 21:56:28 +0900

    bj11047_동전.py

--- 백준문제/Python/그리디/bj11047_동전.py
+++ 백준문제/Python/그리디/bj11047_동전.py
@@ -2,23 +2,32 @@ import sys
 
 n,k = map(int, sys.stdin.readline().rstrip().split())
 coins = []
-
 for _ in range(n):
-    input = int(sys.stdin.readline().rstrip())
-    coins.append(input)
+    t = int(sys.stdin.readline().rstrip())
+    coins.append(t)
 
-coins = reversed(coins)
+coins = list(reversed(coins))
 
 cnt = 0
-tcoin = 1
-while True:
-    for i in range(n):
-        if coins[i] < k:
-            temp = coins[i]
-            break
-    print(k, tcoin,cnt,coins)
-    temp = k//tcoin
-    cnt +=temp
-    k = k%(tcoin*temp)
-    if k == 0 : break
+for i in range(n):
+    coin = coins.pop(0)
+    if coin <= k:
+        t = k // coin
+        cnt += t
+        k -= coin*t
 print(cnt)
+
+# 옛날에 내가 했던 코딩. 학부 2학년
+# cnt = 0
+# tcoin = 1
+# while True:
+#     for i in range(n):
+#         if coins[i] < k:
+#             temp = coins[i]
+#             break
+#     print(k, tcoin,cnt,coins)
+#     temp = k//tcoin
+#     cnt +=temp
+#     k = k%(tcoin*temp)
+#     if k == 0 : break
+# print(cnt)
