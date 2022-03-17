39f96ed4bb9b7a19691064944b3c74bd298abb3f
Jotter-Vortex <ckdwls525@gmail.com>
2022-03-17 16:16:05 +0900

    경로 이름 한글에서 영어로 바꾸기

--- BaekJoon/Python/2022 ICPC Sinchon Winter Algorithm Camp Contest/bj24511_queuestack.py
+++ BaekJoon/Python/2022 ICPC Sinchon Winter Algorithm Camp Contest/bj24511_queuestack.py
@@ -1,11 +1,8 @@
 import sys
-def dynamicpop(list,i, x, l): # l은 식별자. 0이면 큐, 1이면 스택
-    if l == 1:
-        return x
-    else:
-        t = list[i]
-        list[i] = x
-        return t
+def dynamicpop(list,i, x): # l은 식별자. 0이면 큐, 1이면 스택
+    t = list[i]
+    list[i] = x
+    return t
 
 n = int(sys.stdin.readline())
 a = list(map(int, sys.stdin.readline().rstrip().split()))
@@ -14,7 +11,11 @@ m = int(sys.stdin.readline())
 c = list(map(int, sys.stdin.readline().rstrip().split()))
 
 for i in range(m):
-    x = dynamicpop(b,0,c[i],a[0])
+    if a[0] == 0:
+        x = dynamicpop(b,0,c[i])
+    else:
+        x = c[i]
     for j in range(1,n):
-        x = dynamicpop(b,j,x,a[j])
+        if a[j] == 0:
+            x = dynamicpop(b,j,x)
     print(x, end=" ")
