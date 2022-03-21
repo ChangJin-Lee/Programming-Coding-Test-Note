0d5dadf103cf2b6046b9b3ab7807c82a8d8485d7
Jotter-Vortex <ckdwls525@gmail.com>
2022-03-21 22:11:40 +0900

    bj2309_일곱난쟁이.cpp

--- BaekJoon/C++/bj2309_일곱난쟁이.cpp
+++ BaekJoon/C++/bj2309_일곱난쟁이.cpp
@@ -1,3 +1,5 @@
+//노트북에서 공부할땐 안보이던게 데탑 큰 화면으로 보이니까 바로 보였음.
+//확실히 디스플레이는 큰게 장땡이긴 함.
 #include <iostream>
 #include <algorithm>
 
@@ -27,3 +29,8 @@ int main() {
             cout << dwarfs[i] << "\n";
 
 }
+
+//
+// Created by Chang Jin Lee on 2022/03/21.
+//
+
