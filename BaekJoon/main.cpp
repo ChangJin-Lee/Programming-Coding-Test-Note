3b06c2afced7148180728f50620cb64fc25fe1fa
이창진 <54494793+Jotter-Vortex@users.noreply.github.com>
2022-03-22 12:02:00 +0900

    Revert "bj2309_일곱난쟁이.cpp"
    This reverts commit ceee50aa51bfda94c9621f325a7fe199f24acba4.

--- BaekJoon/main.cpp
+++ BaekJoon/main.cpp
@@ -1,70 +1,25 @@
 #include <iostream>
 #include <algorithm>
-#include <string>
 
 using namespace std;
 
-long rev(string x){
-    int i;
-    string result = "";
-    for(i=x.length()-1;i>=0;i--){
-        result+=x[i];
-    }
-    return stoi(result);
-}
-
 int main() {
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-    string x,y;
-    cin >> x >> y;
-    cout << rev(to_string(rev(x) + rev(y)));
-=======
-=======
->>>>>>> parent of 9b29f5d (bj5543_상근날드.cpp)
-=======
->>>>>>> parent of 9b29f5d (bj5543_상근날드.cpp)
-=======
->>>>>>> parent of 9b29f5d (bj5543_상근날드.cpp)
-=======
->>>>>>> parent of 9b29f5d (bj5543_상근날드.cpp)
-=======
->>>>>>> parent of 9b29f5d (bj5543_상근날드.cpp)
-=======
->>>>>>> parent of 9b29f5d (bj5543_상근날드.cpp)
-=======
->>>>>>> parent of 9b29f5d (bj5543_상근날드.cpp)
-    int n[3],i;
-    for(i=0;i<3;i++)
-        cin >> n[i];
-    sort(n,n+3);
-    for(i=0;i<3;i++) cout << n[i] <<" ";
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> parent of 9b29f5d (bj5543_상근날드.cpp)
-=======
->>>>>>> parent of 9b29f5d (bj5543_상근날드.cpp)
-=======
->>>>>>> parent of 9b29f5d (bj5543_상근날드.cpp)
-=======
->>>>>>> parent of 9b29f5d (bj5543_상근날드.cpp)
-=======
->>>>>>> parent of 9b29f5d (bj5543_상근날드.cpp)
-=======
->>>>>>> parent of 9b29f5d (bj5543_상근날드.cpp)
-=======
->>>>>>> parent of 9b29f5d (bj5543_상근날드.cpp)
-=======
->>>>>>> parent of 9b29f5d (bj5543_상근날드.cpp)
+    int i,j,sum=0,t,dwarfs[9];
+    for(i=0;i<9;i++) {
+        cin >> t;
+        dwarfs[i] = t;
+        sum+=t;
+    }
+    sort(dwarfs,dwarfs+9);
+    for(i=0;i<9;i++)
+        for(j=i+1;j<9;j++)
+            if(sum-dwarfs[i]-dwarfs[j] == 100){
+                dwarfs[i] = -1;
+                dwarfs[j] = -1;
+                break;
+            }
+    for(i=0;i<9;i++)
+        if(dwarfs[i] != -1)
+            cout << dwarfs[i] << "\n";
+
 }
