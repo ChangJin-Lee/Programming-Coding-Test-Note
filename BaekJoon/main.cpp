9dfce4ee8d3607c6971bfccdb21fc0d180d4a770
이창진 <54494793+Jotter-Vortex@users.noreply.github.com>
2022-03-22 12:02:14 +0900

    e
    e

--- BaekJoon/main.cpp
+++ BaekJoon/main.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 int main() {
+<<<<<<< HEAD
     int i,j,sum=0,t,dwarfs[9];
     for(i=0;i<9;i++) {
         cin >> t;
@@ -22,4 +23,11 @@ int main() {
         if(dwarfs[i] != -1)
             cout << dwarfs[i] << "\n";
 
+=======
+    int n[3],i;
+    for(i=0;i<3;i++)
+        cin >> n[i];
+    sort(n,n+3);
+    for(i=0;i<3;i++) cout << n[i] <<" ";
+>>>>>>> parent of 9b29f5d (bj5543_상근날드.cpp)
 }
