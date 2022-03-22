2d9cd6d45a2f8b71639584a5396df6213e0dbaee
이창진 <54494793+Jotter-Vortex@users.noreply.github.com>
2022-03-22 12:20:53 +0900

    error 수정

--- BaekJoon/main.cpp
+++ BaekJoon/main.cpp
@@ -1,17 +1,14 @@
 #include <iostream>
 #include <algorithm>
+#include <string>
 
 using namespace std;
 
 int main() {
-    int menu[5],i,j,set[6],k=0;
-    for(i=0;i<5;i++)
-        cin >> menu[i];
-    for(i=0;i<3;i++)
-        for(j=3;j<5;j++) {
-            set[k] = menu[i] + menu[j] - 50;
-            k++;
-        }
-    sort(set,set+6);
-    cout << set[0];
+    int n,m,i,k;
+    cin >> n >> m;
+    if(n>m) k = m;
+    else k=n;
+    for(i=0;i<k;i++)
+        cout << n;
 }
