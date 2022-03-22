94b729d065f073e7502ee7029888a8d9a8f71c7b
changjinlee <54494793+Jotter-Vortex@users.noreply.github.com>
2022-03-22 11:55:35 +0900

    e
    e

--- BaekJoon/main.cpp
+++ BaekJoon/main.cpp
@@ -18,10 +18,13 @@ int main() {
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
     string x,y;
     cin >> x >> y;
     cout << rev(to_string(rev(x) + rev(y)));
 =======
+=======
+>>>>>>> parent of 9b29f5d (bj5543_상근날드.cpp)
 =======
 >>>>>>> parent of 9b29f5d (bj5543_상근날드.cpp)
 =======
@@ -36,6 +39,9 @@ int main() {
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+>>>>>>> parent of 9b29f5d (bj5543_상근날드.cpp)
+=======
 >>>>>>> parent of 9b29f5d (bj5543_상근날드.cpp)
 =======
 >>>>>>> parent of 9b29f5d (bj5543_상근날드.cpp)
