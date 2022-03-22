35af68245b856037602851efa91a3f9cbb226d5a
changjinlee <54494793+Jotter-Vortex@users.noreply.github.com>
2022-03-22 11:50:40 +0900

    e
    e

--- BaekJoon/main.cpp
+++ BaekJoon/main.cpp
@@ -15,14 +15,20 @@ long rev(string x){
 
 int main() {
 <<<<<<< HEAD
+<<<<<<< HEAD
     string x,y;
     cin >> x >> y;
     cout << rev(to_string(rev(x) + rev(y)));
 =======
+=======
+>>>>>>> parent of 9b29f5d (bj5543_상근날드.cpp)
     int n[3],i;
     for(i=0;i<3;i++)
         cin >> n[i];
     sort(n,n+3);
     for(i=0;i<3;i++) cout << n[i] <<" ";
+<<<<<<< HEAD
+>>>>>>> parent of 9b29f5d (bj5543_상근날드.cpp)
+=======
 >>>>>>> parent of 9b29f5d (bj5543_상근날드.cpp)
 }
