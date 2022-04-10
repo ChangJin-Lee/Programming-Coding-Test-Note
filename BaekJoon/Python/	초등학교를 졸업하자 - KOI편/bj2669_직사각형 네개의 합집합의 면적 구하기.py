e8d16cdf2f1c8a16bd6269cd230bedf612d48fbe
Jotter-Vortex <ckdwls525@gmail.com>
2022-04-11 00:00:47 +0900

    bj2669_직사각형 네개의 합집합의 면적 구하기.py - Fixed

--- BaekJoon/Python/	초등학교를 졸업하자 - KOI편/bj2669_직사각형 네개의 합집합의 면적 구하기.py
+++ BaekJoon/Python/	초등학교를 졸업하자 - KOI편/bj2669_직사각형 네개의 합집합의 면적 구하기.py
@@ -1,3 +1,7 @@
+# 그리디로 푸는 대표적인 문제였다.
+# 코드를 자세히 보니까 파이썬 배열의 count()함수는 안쓰는게 좋을거 같다.
+# 시간 복잡도가 너무 크다.
+
 import sys
 
 maps = [[0 for x in range(100)]for s in range(100)]
@@ -5,9 +9,10 @@ for _ in range(4):
     x1,y1,x2,y2 = map(int, sys.stdin.readline().rstrip().split())
     for i in range(x1,x2):
         for j in range(y1,y2):
-            if maps[i][j] !=1:
                 maps[i][j] = 1
 cnt=0
 for i in range(100):
-    cnt+=maps[i].count(1)
+    for j in range(100):
+        if maps[i][j] == 1:
+            cnt+=1
 print(cnt)
