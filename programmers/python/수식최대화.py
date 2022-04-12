33b72867d06ead740c2d5536aac45a276d6dc75b
Jotter-Vortex <ckdwls525@gmail.com>
2022-04-12 14:24:53 +0900

    수식최대화.py

---
+++ programmers/python/수식최대화.py
@@ -0,0 +1,39 @@
+import itertools
+import copy
+expset = ["+","-","*"]
+def solution(expression):
+    answer = 0
+    expression = list(expression)
+    priority = list(itertools.permutations(expset,3))
+    exp = []
+    for i in range(len(expression)):
+        if expression[i] in expset:
+            exp.append(expression[i])
+            expression[i] = " "
+    numbers = list(map(int,"".join(expression).split(" ")))
+    # print(numbers, exp, priority)
+    for ex in priority:
+        cnumbers = copy.deepcopy(numbers)
+        cexp = copy.deepcopy(exp)
+        for i in range(3):
+            j=0
+            while True:
+                if j >= len(cexp):
+                    break
+                if cexp[j] == ex[i]:
+                    if cexp[j] == "-":
+                        cnumbers[j] -= cnumbers[j+1]
+                    elif cexp[j] == "+":
+                        cnumbers[j] += cnumbers[j+1]
+                    elif cexp[j] == "*":
+                        cnumbers[j] *= cnumbers[j+1]
+                    cnumbers.pop(j+1)
+                    cexp.pop(j)
+                else:
+                    j+=1
+        if abs(cnumbers[0]) > answer:
+            answer = abs(cnumbers[0])
+    return answer
+
+print(solution("100-200*300-500+20")) ## 60420
+print(solution("50*6-3*2")) ## 300
