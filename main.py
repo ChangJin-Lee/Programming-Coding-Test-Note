be00ba3edffdd57559aa0c8ac73b3fb41f7d3729
Jotter-Vortex <ckdwls525@gmail.com>
2022-07-09 17:07:02 +0900

    2022.07.09

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
-import heapq
-a = []
-heapq.heapify(a)
-print(a)
-# print(heapq.heappop(a))
+A  = [5,1,3,7]
+B = [2,2,6,6]	
+answer = 0
+focustable = []
+A.sort(reverse=True)
+B.sort(reverse=True)
+print(A,B)
+
+for i in range(len(A)):
+    if A[i] < B[i]:
+        answer += 1
+    elif A[i] > B[i]:
+        for j in range(i+1,len(A)):
+            if A[j] > B[j] and A[j] < B[i]:
+                B[i],B[j] = B[j],B[i]
+                break
+print(A,B)
+print(answer)
