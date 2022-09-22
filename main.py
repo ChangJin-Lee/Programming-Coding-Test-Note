e5b3410359f3fd3e6a4db7b6ed09ac28f79102df
Jotter-Vortex <ckdwls525@gmail.com>
2022-09-22 21:39:03 +0900

    (update) 베스트앨범

--- main.py
+++ main.py
@@ -8,21 +8,20 @@ def solution(genres, plays):
     for (genre, play) in zip(genres, plays):
         dic[genre].update({plays.index(play): play})
 
-    print(dic)
     for m in dic.items():
         total = sum((m[1].values()))
         totaldic[m[0]] = total
-
     genre_rankings = sorted(totaldic, key=lambda x: x[1], reverse=True)
 
     for genre_ranking in genre_rankings:
-        tmp = dic[genre_ranking]
-        print(tmp)
-        song_rankings = sorted(tmp, key=lambda x: x[1], reverse=True)
-        print(song_rankings)
+        tmp = dict(dic[genre_ranking])
+        song_rankings = sorted(
+            tmp.items(), key=lambda x: x[1], reverse=True)[0:2]
+        for k in song_rankings:
+            answer.append(k[0])
     return answer
 
 
 if __name__ == "__main__":
-    solution(["classic", "pop", "classic", "classic", "pop"],
-             [500, 600, 150, 800, 2500])
+    print(solution(["classic", "pop", "classic", "classic", "pop"],
+                   [500, 600, 150, 800, 2500]))
