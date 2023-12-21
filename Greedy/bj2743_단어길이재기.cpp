02b04d6edac8a1a24555f2921a28dfa809a819f4
ChangJin-Lee <ckdwls525@gmail.com>
2023-12-21 14:39:16 +0900

    update: bj2743_단어길이재기.cpp

---
+++ Greedy/bj2743_단어길이재기.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#define fastcpp ios::sync_with_stdio(0), cin.tie(nullptr), cout.tie(nullptr)
+using namespace std;
+
+int main()
+{
+    fastcpp;
+    string input;
+    cin >> input;
+    cout << input.size();
+}
